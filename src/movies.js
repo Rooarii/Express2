21724f5dc0183609f80cc3382ca2832090737112
Rooarii <rooarii.manuel@gmail.com>
2020-11-30 23:47:05 +0100

    api/movies route connected to db

---
+++ src/movies.js
@@ -0,0 +1,36 @@
+const movies = [
+  {
+    id: 0,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    colors: false,
+    duration: 120
+  },
+  {
+    id: 1,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    colors: true,
+    duration: 180
+  },
+  {
+    id: 2,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180
+  },
+  {
+    id: 3,
+    title: "Matrix",
+    director: "Rooarii",
+    year: "1989",
+    color: true,
+    duration: 100
+  }
+];
+
+module.exports = movies;
